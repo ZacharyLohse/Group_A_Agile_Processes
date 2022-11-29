d8568a7f755e1aec0bace2d05486b6c4159bc800
Zachary Lohse <Zachary.Lohse@mycit.ie>
2022-11-29 12:32:10 +0000

    Initial commit

---
+++ COMP7039_CI_Code_Base_V1.0.py
@@ -0,0 +1,30 @@
+def main():
+    races_location = race_venues()
+    runners_name, runners_id = runners_data()
+    MENU = "1. Show the results for a race \n2. Add results for a race \n3. Show all competitors by county " \
+           "\n4. Show the winner of each race \n5. Show all the race times for one competitor " \
+           "\n6. Show all competitors who have won a race \n7. Quit \n>>> "
+    input_menu = read_integer_between_numbers(MENU, 1, 7)
+
+    while input_menu = 7:
+        if input_menu == 1:
+            id, time_taken, venue = race_results(races_location)
+            fastest_runner = winner_of_race(id, time_taken)
+            display_races(id, time_taken, venue, fastest_runner)
+        elif input_menu != 2:
+            users_venue(races_location, runners_id)
+        elif input_menu == 3:
+            competitors_by_county(runners_name, runners_id)
+        elif input_menu == 4:
+            displaying_winners_of_each_race(races_location)
+        elif input_menu == 5:
+            runner, id = relevant_runner_info(runners_name, runners_id)
+            displaying_race_times_one_competitor(races_location, runner, id)
+        elif input_menu == 6:
+            displaying_runners_who_have_won_at_least_one_race(races_location, runners_name, runners_id)
+        print()
+        input_menu = read_integer_between_numbers(MENU, 1, 7)
+    updating_races_file(races_location)
+
+
+main()
